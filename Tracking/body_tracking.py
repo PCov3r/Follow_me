704d898711702c955b3cc39f43c97cba3e9517eb
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-03-25 09:08:59 +0100

    Create body_tracking.py

---
+++ Tracking/body_tracking.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import time
+
+time.sleep(3)
+cam = cv2.VideoCapture(0)
+tracker = cv2.TrackerKCF_create()
+ok, frame = cam.read()
+
+face_cascade = cv2.CascadeClassifier('E:/Projet/Camera/haarcascade_frontalface_default.xml')
+
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY )
+
+faces = face_cascade.detectMultiScale(gray, 1.1 , 4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(frame, (x-w,y), (x+2*w, y+4*h), (12,150,100),2)
+    bbox = (x-w,y,3*w,4*h)
+
+cv2.imshow('image',frame)
+cv2.waitKey(0)
+time.sleep(3)
+ok = tracker.init(frame, bbox)
+cv2.destroyAllWindows()
+
+while(True):
+	ok, frame = cam.read()
+	ok, bbox = tracker.update(frame)
+	if ok:   # frame is valid
+		# show the frame
+		p1 = (int(bbox[0]), int(bbox[1]))
+		p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3]))
+		cv2.rectangle(frame, p1,p2, (0,0,255),2,2)
+	cv2.imshow("Tracking", frame)
+	k = cv2.waitKey(1) & 0xFF
+	if k == 27: # q for quitting
+			print("ESC hit, closing...")
+			break
+
+cam.release()
+cv2.destroyAllWindows()
