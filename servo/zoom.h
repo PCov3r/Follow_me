115407704213427fd022f84360c8671840c74ddb
Reivax83 <81352344+Reivax83@users.noreply.github.com>
2021-05-27 11:07:31 +0200

    Update zoom.h

--- servo/zoom.h
+++ servo/zoom.h
@@ -12,14 +12,18 @@
 #define Slave_Adress  		0xc
 #define Slave_Adress_7b		0x18
 #define Slave_Zoom_Adress	0x00
+#define Slave_Focus_Adress	0x01
 #define Mem_Size			0x01
 #define Zoom_Increment_1	0x01
 #define Zoom_Increment_2	0x00
-
+#define Focus_Increment_1	0x01
+#define Focus_Increment_2	0x00
 
 #define BUFFER_i2c_tx_size 3
 #define BUFFER_i2c_rx_size 2
 
 void zoomrel( int sens);
+
+void focusrel( int sens, int value);
 
 #endif /* SRC_ZOOM_H_ */
