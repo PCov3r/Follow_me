c57f29355c98c6724ab118078ee0b2cbc099d77b
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-12 13:13:03 +0200

    Update video.py

--- WebServer/FlaskServer/video.py
+++ WebServer/FlaskServer/video.py
@@ -5,12 +5,12 @@ import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False): #On lance l'aquisition du flux vidéo sans retournement d'image. 
+    def __init__(self, flip = False): #On lance l'aquisition du flux vidéo avec possibilité d'inverser l'image (valeur par défaut : False) 
         self.vs = PiVideoStream().start()
         self.flip = flip
         time.sleep(2.0)
 
-    def __del__(self): #On stop l'aquisition du flux vidéo.
+    def __del__(self): #On stoppe l'aquisition du flux vidéo.
         self.vs.stop()
 
     def flip_if_needed(self, frame):  #On retourne l'image si nécessaire. 
