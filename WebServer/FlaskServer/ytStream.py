e55ad8c1149fc26ff117a1fdc515fd2e17d18211
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-12 13:24:40 +0200

    Update ytStream.py

--- WebServer/FlaskServer/ytStream.py
+++ WebServer/FlaskServer/ytStream.py
@@ -1,12 +1,17 @@
+# Script permettant de lancer le stream via YT
+# Pour cela il faut créer une clé de stream depuis son compte et la coller dans KEY
+# /!\ Ce script utilise la librairie picamera et n'est donc pas pour le moment utilisable avec le script Flask générant la page web : main.py /!\
+
 import subprocess 
 import picamera 
 import time 
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
-KEY= #ENTER PRIVATE KEY HERE# 
+KEY= #La clé va ici# 
+# On lance le stream
 stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:1,0 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 
-try: 
+try: # On lance l'acquisition caméra
   now = time.strftime("%Y-%m-%d-%H:%M:%S") 
   camera.framerate = 25 
   camera.vflip = True 
@@ -14,11 +19,10 @@ try:
   camera.start_recording(stream.stdin, format='h264', bitrate = 2000000) 
   while True: 
      camera.wait_recording(1) 
-except KeyboardInterrupt: 
+except KeyboardInterrupt: # ctrl+C pour arrêter la caméra
      camera.stop_recording() 
-finally: 
+finally: # Fermeture du stream 
   camera.close() 
   stream.stdin.close() 
   stream.wait() 
-  print("Camera safely shut down") 
-  print("Good bye") 
+  print("Caméra éteinte et stream coupé") 
