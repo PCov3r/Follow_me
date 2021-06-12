000780724a5b31e5a17fed771f6156396830a8b4
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-12 13:05:19 +0200

    Update video.py

--- WebServer/FlaskServer/video.py
+++ WebServer/FlaskServer/video.py
@@ -1,7 +1,3 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This scrtipt script..
-
 import cv2
 from imutils.video.pivideostream import PiVideoStream
 import imutils
@@ -9,20 +5,20 @@ import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False):
+    def __init__(self, flip = False): #On lance l'aquisition du flux vidéo sans retournement d'image. 
         self.vs = PiVideoStream().start()
         self.flip = flip
         time.sleep(2.0)
 
-    def __del__(self):
+    def __del__(self): #On stop l'aquisition du flux vidéo.
         self.vs.stop()
 
-    def flip_if_needed(self, frame):
+    def flip_if_needed(self, frame):  #On retourne l'image si nécessaire. 
         if self.flip:
             return np.flip(frame, 0)
         return frame
 
-    def get_frame(self):
+    def get_frame(self):    #On récupère le flux vidéo. 
         frame = self.flip_if_needed(self.vs.read())
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
