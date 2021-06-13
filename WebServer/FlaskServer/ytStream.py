5671bd8a6415dc2f4de0fbf0b3a30b3661af1aec
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 23:05:14 +0200

    Update ytStream.py

--- WebServer/FlaskServer/ytStream.py
+++ WebServer/FlaskServer/ytStream.py
@@ -1,17 +1,18 @@
-# Script permettant de lancer le stream via YT
-# Pour cela il faut créer une clé de stream depuis son compte et la coller dans KEY
-# /!\ Ce script utilise la librairie picamera et n'est donc pas pour le moment utilisable avec le script Flask générant la page web : main.py /!\
+# Script to launch the stream via YT
+# For that you have to create a stream key from your account and paste it in KEY
+# /!\ This script uses the picamera library and is therefore not currently usable with the Flask script generating the web page: main.py /!\
+# Script from https://learn.adafruit.com/ultimate-youtube-live-camera/streaming-to-youtube
 
 import subprocess 
 import picamera 
 import time 
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
-KEY= #La clé va ici# 
-# On lance le stream
+KEY= # Key goes here #
+# Launching live streaming
 stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:1,0 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 
-try: # On lance l'acquisition caméra
+try: # Launching camera
   now = time.strftime("%Y-%m-%d-%H:%M:%S") 
   camera.framerate = 25 
   camera.vflip = True 
@@ -19,10 +20,10 @@ try: # On lance l'acquisition caméra
   camera.start_recording(stream.stdin, format='h264', bitrate = 2000000) 
   while True: 
      camera.wait_recording(1) 
-except KeyboardInterrupt: # ctrl+C pour arrêter la caméra
+except KeyboardInterrupt: # Wait for crtl+C to stop camera
      camera.stop_recording() 
-finally: # Fermeture du stream 
+finally: # Closing live stream
   camera.close() 
   stream.stdin.close() 
   stream.wait() 
-  print("Caméra éteinte et stream coupé") 
+  print("Live stream shutdown") 
