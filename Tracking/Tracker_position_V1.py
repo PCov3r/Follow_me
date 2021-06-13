f146a31c5b511416534d3325006e0287c7ab0e5f
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 23:48:06 +0200

    Update Tracker_position_V1.py

--- Tracking/Tracker_position_V1.py
+++ Tracking/Tracker_position_V1.py
@@ -1,5 +1,5 @@
-#Il s'agit d'un script de tracking de visage non adapté à la raspberry. Dans lequel, on affiche une image vue par la caméra avec la position du tracker. ON la valide si elle nous 
-#convient en appuyant sur entrer. Ensuite, on affiche le flux video de la caméra de l'ordinateur avec la position du tracker. 
+#This is a face tracking script not suitable for the raspberry. We display an image seen by the camera with the position of the tracker. We validate it if we
+# find if convenient by pressing enter. Then, we display the video stream from the computer camera with the position of the tracker.
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -10,32 +10,32 @@ cam = cv2.VideoCapture(0)
 tracker = cv2.TrackerKCF_create()
 ok, frame = cam.read()
 
+# Tracking file
 face_cascade = cv2.CascadeClassifier('E:/Projet/Camera/haarcascade_frontalface_default.xml')
-#On récupère le fichier de tracking du visage
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY )
-#On convertit les couleurs de l'image en niveau de gris pour faciliter la détection du corps du professeur. 
+# Image get converted to gray scale to ease detection
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY ) 
 faces = face_cascade.detectMultiScale(gray, 1.1 , 4)
-#On récupère les coordonnées du visage du professeur.
+# A face has been detected
 for (x,y,w,h) in faces:
     cv2.rectangle(frame, (x-w,y), (x+2*w, y+4*h), (12,150,100),2)
-    bbox = (x-w,y,3*w,4*h)  # Si visage, on renvoie un tableau contenant les coordonnées et dimensions du haut du corps
+    bbox = (x-w,y,3*w,4*h)  # bbox contains the upper part of the body
 
-cv2.imshow('image',frame) #On affiche la frame qui est une fenêtre de "validation" pour  voir si le tracking à bien été réalisé.
-cv2.waitKey(0)  #On doit appuyer sur entrer pour valider la position du rectangle.
+cv2.imshow('image',frame) # Showing detected face
+cv2.waitKey(0)  # User must hit enter to validate tracker position
 time.sleep(3)
 ok = tracker.init(frame, bbox) 
-cv2.destroyAllWindows() #On écrase cette fenêtre de "validation"  
+cv2.destroyAllWindows() # We close this "validation" window 
 
 while(True):
-	ok, frame = cam.read() # On lit une frame
-	ok, bbox = tracker.update(frame) # On met à jour le tracker
-	if ok:   # Si on détecte un visage on affiche le rectangle correspondant. 
+	ok, frame = cam.read() # Frame reading
+	ok, bbox = tracker.update(frame) # Tracker updating
+	if ok:   # If a face is detected, we print the bounding box
 		p1 = (int(bbox[0]), int(bbox[1]))
 		p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3]))
 		cv2.rectangle(frame, p1,p2, (0,0,255),2,2)
-	cv2.imshow("Tracking", frame) # En même temps, on affiche l'image de la caméra pour avoir un retour
+	cv2.imshow("Tracking", frame) # Show video
 	k = cv2.waitKey(1) & 0xFF
-	if k == 27: # echap pour quitter
+	if k == 27: # esc to quit
 			print("ESC hit, closing...")
 			break
 
