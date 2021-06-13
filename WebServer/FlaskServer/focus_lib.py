31819de2630bbd7311a545cd1c122ef30b44210f
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 22:46:37 +0200

    Update focus_lib.py

--- WebServer/FlaskServer/focus_lib.py
+++ WebServer/FlaskServer/focus_lib.py
@@ -1,6 +1,6 @@
-# Script gérant le focus automatique #
-# /!\ Ce dernier n'est pour le moment pas fonctionnel car utilise la librairie picamera au lieu de la librairie créée camera.py
-# Largement inspiré de https://www.arducam.com/raspberry-pi-camera/autofocus/#next-level-raspberry-pi-camera-autofocus-with-ptz-pan-tilt-zoom-cameras
+# Auto focus script #
+# /!\ The latter is currently not functional because it uses the picamera library instead of the custom camera.py library
+# Largely inspired by https://www.arducam.com/raspberry-pi-camera/autofocus/#next-level-raspberry-pi-camera-autofocus-with-ptz-pan-tilt-zoom-cameras
 
 import cv2 
 import numpy as py
@@ -9,20 +9,20 @@ from uartcom import sendData
 from camera import VideoCamera
 from picamera.array import PiRGBArray
 	
-def focusing(val): # Fonction générant les données en hexa à envoyer à la carte gérant la caméra
+def focusing(val): # Function generating the data in hexa to send to the card managing the camera
 	value = (val << 4) & 0x3ff0
 	data1 = (value >> 8) & 0x3f
 	data2 = value & 0xf0
 	return([data1,data2])  
 
 
-def laplacian(img): # Application du Laplacien sur l'image (détection de contour)
+def laplacian(img): # Use laplacian on image to detect edges
 	img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 	img_sobel = cv2.Laplacian(img_gray,cv2.CV_16U)
 	return cv2.mean(img_sobel)[0]
 	
 
-def calculation(camera): # Acquisition d'une frame de la caméra et recherche des contours
+def calculation(camera): # Search for edges in frame
 	rawCapture = PiRGBArray(camera) 
 	camera.capture(rawCapture,format="bgr", use_video_port=True)
 	image = rawCapture.array
@@ -40,33 +40,33 @@ def setFocus(camera) :
 	focal_distance = 10
 
 	while True:
-	    # Ajustement du focus
-		[F1,F2] = focusing(focal_distance) # On sa place tout au bout de la distance focale
-		sendData([255,255,255,F1,F2]) # On envoie à la carte gérant les servos la commande de la distance focale (et donc on ne touche pas à la postion en x, y ou au zoom)
+	    # Adjust focus
+		[F1,F2] = focusing(focal_distance) # Focus on maximum distance
+		sendData([255,255,255,F1,F2]) # Send focus to STM32 via Uart (x, y and zoom untouched)
 		#Take image and calculate image clarity
 		frame = camera.get_frame()
 		val = calculation(frame)
-		#Recherche du focus en cherchant la valeur pour laquelle on distingue un maximum de contours
+		# Searching for best focus, ie the one whose camera frame has clear edges
 		if val > max_value:
 			max_index = focal_distance
 			max_value = val
 			
-		# Si la clarté de l'image diminue
+		# Image clarity is decreasing
 		if val < last_value:
 			dec_count += 1
 		else:
 			dec_count = 0
-		# Si la clarté diminue de manière consécutive 6 fois, on a trouvé le focus idéal
+		# Image clarity decrease 6 times in a row, meaning we found the best focus
 		if dec_count > 6:
 			break
 		last_value = val
 		
-		# Augmentation de la distance focale
+		# Testing other focal distances
 		focal_distance += 15
 		if focal_distance > 1000:
 			break
 
-    # Réglage sur le focus
+    # Set focus
 	[F1,F2] = focusing(max_index)
 	sendData([255,255,255,F1,F2])
 	return(0)
