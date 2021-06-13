669d39155cfb075214e489e284e30e01e649a835
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 22:22:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ sudo pip3 install opencv-contrib-python==4.4.0.46
 sudo pip3 install Flask
 sudo pip3 install imutils
 sudo apt-get install python-picamera python3-picamera
+sudo pip3 install pyserial
+sudo pip3 install pycurl
