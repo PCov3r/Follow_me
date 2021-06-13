ebb3f76e0c3a9be2f35b330d61ecac4db0125cbb
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-14 00:23:55 +0200

    Update Tracker_position_V3.py

--- Tracking/Tracker_position_V3.py
+++ Tracking/Tracker_position_V3.py
@@ -9,98 +9,96 @@ import time
 #ok, frame = cam.read()
 
 
-## Cette fonction initialise le tracker
-
+## Initialize tracker
 def init(param):
-	bounding = (0,0,0,0) #On initialise le tableau contenant les coordonnées du haut du corps. 
+	bounding = (0,0,0,0) # bounding will contain the upper part of the detected body 
 
 	face_cascade = cv2.CascadeClassifier('C:/Users/pauld/Documents/Machine_virtuelle/Projet/Camera/haarcascade_frontalface_default.xml')
-	#On récupère le fichier de tracking du visage 
+	# Convert to gray scale to ease detection
 	gray = cv2.cvtColor(param, cv2.COLOR_BGR2GRAY )
-	#On convertit les couleurs de l'image en niveau de gris pour faciliter la détection du corps du professeur. 
 	faces = face_cascade.detectMultiScale(gray, 1.1 , 4)
-	#On récupère les coordonnées du visage du professeur.  
+	# Get coordinates, width and heigth of the detected face
 	for (x,y,w,h) in faces:
 		#cv2.rectangle(param, (x-w,y), (x+2*w, y+4*h), (12,150,100),2)
-		bounding = (x-w,y,3*w,4*h) # Si visage, on renvoie un tableau contenant les coordonnées et dimensions du haut du corps
+		bounding = (x-w,y,3*w,4*h)  # bounding contains the coordinates and size of the upper part of the body
 		print("visage trouvé")
 
 	return(bounding)
 
-## Fonction principale de tracking
+## Main function
 
 def tracking():
-    cam = cv2.VideoCapture(0) # On démarre la caméra
-    tracker = cv2.TrackerKCF_create() # On crée un tracker
-	ok, frame = cam.read() # On lit une frame
-	bbox = init(frame) # On essaye d'y trouver une personne
+    cam = cv2.VideoCapture(0) # Start camera
+    tracker = cv2.TrackerKCF_create() # Create tracker
+	ok, frame = cam.read() # Read frame
+	bbox = init(frame) # Try to find face
 	print(bbox)
-	while bbox == (0,0,0,0) : # Si on a aucune personne, on recommence la recherche en boucle
+	while bbox == (0,0,0,0) : # If none, loop
         ok, frame = cam.read()
         bbox=init(frame)
-        cv2.imshow("Tracking", frame) # En même temps, on affiche l'image de la caméra pour avoir un retour
+        cv2.imshow("Tracking", frame) # Continue to show video
 		k = cv2.waitKey(1) & 0xFF
-		if k == 27: # echap for quitting
+		if k == 27: # esc for quitting
 			print("ESC hit, closing...")
 			cam.release()
 			cv2.destroyAllWindows()
-	ok = tracker.init(frame, bbox) # Une fois la personne trouvé, on lance le tracking
+	ok = tracker.init(frame, bbox) # Face found, launch tracking
 	exit=0
 	a=0
 
-	while(not exit): # Tant que l'utilisateur n'a pas pressé Echap, on recommence
+	while(not exit): # While esc not pressed
         ok, frame = cam.read()
-		ok, bbox = tracker.update(frame) # On met à jour le tracker avec le flux video
-		if ok:   # La personne est bien détectée
-			p1 = (int(bbox[0]), int(bbox[1])) # On récupère les coordonnées du rectangle de tracking
-			p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3])) # On récupère la largeur et hauteur du rectangle
+		ok, bbox = tracker.update(frame) # Update tracker
+		if ok:   # Still detected
+			p1 = (int(bbox[0]), int(bbox[1])) # Get coordinates of bounding box
+			p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3])) # Get width and length of box
 			cam_width = cam.get(3)
-			cam_height = cam.get(4) # On récupère les dimensions de la vidéo
-			xpos = int(cam_width/2-(bbox[0]+bbox[2]/2)) #Récupère la position par rapport au centre (largeur)
-			ypos = -int(cam_height/2-(bbox[1]+bbox[3]/2)) #Récupère la position par rapport au centre (hauteur)
+			cam_height = cam.get(4) # Get camera resolution
+			xpos = int(cam_width/2-(bbox[0]+bbox[2]/2)) # Get position from center (x axis)
+			ypos = -int(cam_height/2-(bbox[1]+bbox[3]/2)) # Get position from center (y axis)
 			zoom = 0
-			if(bbox[3]>3/4*cam_height): # Si le rectangle de détection occupe plus des 3/4 de la hauteur, on doit dézoomer
+			if(bbox[3]>3/4*cam_height): # If rectangle greater than 3/4 of the screen, we need to unzoom
 				zoom=-1
-			elif(bbox[3]<1/2*cam_height): # Si le rectangle de détection occupe moins des 1/2 de la hauteur, on doit zoomer
+			elif(bbox[3]<1/2*cam_height): # If rectangle less than 1/2 of the screen, we need to zoom
 				zoom=1
 			else:
 				zoom=0
-			print(xpos,ypos,zoom) # On récupère toutes ces données afin de les envoyer à la STM32
-			cv2.rectangle(frame, p1,p2, (0,0,255),2,2) # On affiche le rectangle autour de la personne détectée
-			cv2.imshow("Tracking", frame) # On affiche la vidéo
+			print(xpos,ypos,zoom) # Print all the data
+			cv2.rectangle(frame, p1,p2, (0,0,255),2,2) # Show bounding box
+			cv2.imshow("Tracking", frame) # Show video
 
 		t0=time.time()
-		while (not ok): # On a perdu la personne
+		while (not ok): # Face lost
 			ok, frame = cam.read()
 
-			if(time.time()-t0)>5: # Si cela fait plus de 5s, il faut relancer le tracking
-				tracker = cv2.TrackerKCF_create() # On recrée un tracker
+			if(time.time()-t0)>5: # If face lost for more than 5s, we need to relaunch tracking
+				tracker = cv2.TrackerKCF_create() # Recreate a tracker
 				bbox =init(frame)
 				while bbox == (0,0,0,0) :
 					k = cv2.waitKey(1) & 0xFF
-					if k == 27: # si l'utilisateur appuie sur ESC, on quitte le progamme 
+					if k == 27: # esc for quitting 
 						print("ESC hit, closing...")
 						a=1
 						exit=1
 						break
 					ok, frame = cam.read()
 					bbox=init(frame)
-					cv2.imshow("Tracking", frame) # On essaie de retrouver la personne
+					cv2.imshow("Tracking", frame) # Continue to show video
 				if(a) :
 					break
-				ok = tracker.init(frame, bbox) # On réinitialise le tracker lorsqu'on a trouvé la personne
+				ok = tracker.init(frame, bbox) # Reinitialize tracker
 
-			ok, bbox = tracker.update(frame) # On met à jour le tracker
+			ok, bbox = tracker.update(frame) # Update tracker
 			cv2.imshow("Tracking", frame)
 			k = cv2.waitKey(1) & 0xFF
-			if k == 27: # q for quitting
+			if k == 27: # esc for quitting
 				print("ESC hit, closing...")
 				exit=1
 				break
 
 
 		k = cv2.waitKey(1) & 0xFF
-		if k == 27: # si l'utilisateur appuie sur ESC, on quitte le progamme
+		if k == 27: # esc for quitting
 			print("ESC hit, closing...")
 			exit=1
 
