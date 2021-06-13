3424e6120215d903d1ff586773a2b9ed6be51aa1
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 22:38:44 +0200

    Update camera.py

--- WebServer/FlaskServer/camera.py
+++ WebServer/FlaskServer/camera.py
@@ -1,4 +1,4 @@
-# Script des fonctions l'acquisition du flux vidéo #
+# Camera functions #
 
 import cv2
 from imutils.video.pivideostream import PiVideoStream
@@ -7,19 +7,19 @@ import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False): #On lance l'aquisition du flux vidéo avec possibilité d'inverser l'image (valeur par défaut : False) 
+    def __init__(self, flip = False): # Initiate video stream and flip image if needed (default: False) 
         self.vs = PiVideoStream().start()
         self.flip = flip
         time.sleep(2.0)
 
-    def __del__(self): #On stoppe l'aquisition du flux vidéo.
+    def __del__(self): # Stop video stream
         self.vs.stop()
 
-    def flip_if_needed(self, frame):  #On retourne l'image si nécessaire. 
+    def flip_if_needed(self, frame):  # Flip camera
         if self.flip:
             return np.flip(frame, 0)
         return frame
 
-    def get_frame(self):    #On récupère le flux vidéo. 
+    def get_frame(self):    # Get camera frame
         frame = self.flip_if_needed(self.vs.read())
         return frame
