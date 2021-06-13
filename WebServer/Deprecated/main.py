83ced469009c4073e1afe3b83bf292f24b632843
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 22:48:43 +0200

    Update main.py

--- WebServer/Deprecated/main.py
+++ WebServer/Deprecated/main.py
@@ -1,6 +1,3 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response, request
@@ -9,23 +6,23 @@ import time
 import threading
 import os
 
-pi_camera = VideoCamera(flip=False) # flip pi camera si nécessaire.
+pi_camera = VideoCamera(flip=False) # flip pi camera if necessary
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
-@app.route('/')  #Page d'acceuil du serveur Flask
+@app.route('/')  # Main page of Flask server
 def index():
     return render_template('index.html')
 
 def gen(camera):
     #get camera frame
     while True:
-        frame = camera.get_frame() #On lit une frame
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed') #Chemin du flux vidéo
+@app.route('/video_feed') # Video streaming
 def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
