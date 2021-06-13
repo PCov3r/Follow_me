84b3a3e536bead13f05931cadf8b8dea73c169b8
Reivax83 <81352344+Reivax83@users.noreply.github.com>
2021-06-13 16:04:06 +0200

    Update zoom.h

--- servo/zoom.h
+++ servo/zoom.h
@@ -1,39 +1,33 @@
-/*
- * zoom.h
- *
- *  Created on: 27 mai 2021
- *      Author: xavier
- */
-
 #include "main.h"
 
 #ifndef SRC_ZOOM_H_
 #define SRC_ZOOM_H_
 
-// Adresses
+// Addresses
 
-#define Slave_Adress  		  0xc
-//adresse de la caméra décalée d'un bit vers la gauche
-#define Slave_Adress_7b		  0x18
-//adresse des registres de la caméra
-#define Slave_Zoom_Adress	  0x00
-#define Slave_Focus_Adress	0x01
+#define Slave_Address		0xc
+//address of the camera shifted to the left by one bit
+#define Slave_Address_7b	0x18
+//addresses of the register in the camera
+#define Slave_Zoom_Address	0x00
+#define Slave_Focus_Address	0x01
 
-#define Mem_Size			      0x01
+// Memory Size in byte
+#define Mem_Size			0x01
 
-// Incréments
+// Increments
 
-#define Zoom_Increment_1	  0x01
-#define Zoom_Increment_2	  0x00
-#define Focus_Increment_1	  0x01
-#define Focus_Increment_2	  0x00
+#define Zoom_Increment_1	0x01
+#define Zoom_Increment_2	0x00
+#define Focus_Increment_1	0x01
+#define Focus_Increment_2	0x00
 
-// Définition des tableaux servant à la transmition
+// definition of the tables use for transmission
 
-#define BUFFER_i2c_tx_size  3
-#define BUFFER_i2c_rx_size  2
+#define BUFFER_i2c_tx_size 	3
+#define BUFFER_i2c_rx_size 	2
 
-// Fonctions
+// Functions
 
 void zoomrel( I2C_HandleTypeDef* i2c, int sens);
 void zoom(I2C_HandleTypeDef* i2c, int val12, int val34);
