22587db8017797e24a08c2acb54594821535bb81
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-14 00:05:37 +0200

    Update Tracker_position_V2.py

--- Tracking/Tracker_position_V2.py
+++ Tracking/Tracker_position_V2.py
@@ -7,67 +7,66 @@ import time
 #tracker = cv2.TrackerKCF_create()
 #ok, frame = cam.read()
 
-## Cette fonction initialise le tracker
+## Function to initialize tracker
 
 def init(param):
-	bounding = (0,0,0,0) #On initialise le tableau contenant les coordonnées du haut du corps. 
+	bounding = (0,0,0,0) # bounding will contain the upper part of the detected body 
 
 	face_cascade = cv2.CascadeClassifier('C:/Users/pauld/Documents/Machine_virtuelle/Projet/Camera/haarcascade_frontalface_default.xml')
-	#On récupère le fichier de tracking du visage 
+	# Convert to gray scale to ease detection
 	gray = cv2.cvtColor(param, cv2.COLOR_BGR2GRAY )
-	#On convertit les couleurs de l'image en niveau de gris pour faciliter la détection du corps du professeur. 
 	faces = face_cascade.detectMultiScale(gray, 1.1 , 4)
-	#On récupère les coordonnées du visage du professeur.
+	# Get coordinates, width and heigth of the detected face
 	for (x,y,w,h) in faces:
 		#cv2.rectangle(param, (x-w,y), (x+2*w, y+4*h), (12,150,100),2)
-		bounding = (x-w,y,3*w,4*h)  # Si visage, on renvoie un tableau contenant les coordonnées et dimensions du haut du corps
+		bounding = (x-w,y,3*w,4*h)  # bounding contains the coordinates and size of the upper part of the body
 		print("visage trouvé")
 
 	return(bounding)
 
-## Fonction principale de tracking 
+## Main tracking function 
 
 def tracking():
-    cam = cv2.VideoCapture(0) #On démarre la caméra
-    tracker = cv2.TrackerKCF_create() # On crée un tracker
-	ok, frame = cam.read() # On lit une frame. 
-	bbox = init(frame) # On essaye de trouver une personne dans la frame.
+    cam = cv2.VideoCapture(0) # Starting camera
+    tracker = cv2.TrackerKCF_create() # Create tracker
+	ok, frame = cam.read() # Read frame 
+	bbox = init(frame) # Find face in the frame
 	print(bbox)
-	while bbox == (0,0,0,0) : # Tant qu'on ne trouve personne, on recommence la recherhe.
-        ok, frame = cam.read()
-        bbox=init(frame)
-        cv2.imshow("Tracking", frame) # En même temps, on affiche l'image de la caméra pour avoir un retour.
+	while bbox == (0,0,0,0) : # While no face detected
+            ok, frame = cam.read()
+            bbox=init(frame)
+            cv2.imshow("Tracking", frame) # Keep showing video stream while searching
 	ok = tracker.init(frame, bbox)
 
 	while(True):
         ok, frame = cam.read()
-		ok, bbox = tracker.update(frame) # On met à jour le tracker avec le flux vidéo
-		if ok:   # La personne est bien détectée
-			print("c ok")
-			p1 = (int(bbox[0]), int(bbox[1])) # On récupère les coordonnées du rectangle de tracking
-			p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3])) # On récupère la largeur et hauteur du rectangle
-			cv2.rectangle(frame, p1,p2, (0,0,255),2,2) # On affiche le rectangle autour de la personne détectée
-			cv2.imshow("Tracking", frame)  # On affiche la vidéo
+		ok, bbox = tracker.update(frame) # Update tracker position in frame
+		if ok:   # Face is still detected
+			print("ok")
+			p1 = (int(bbox[0]), int(bbox[1])) # Get tracking rectangle coordinates
+			p2 = (int(bbox[0]+bbox[2]),int(bbox[1]+bbox[3])) # Get width and length of tracking rectangle
+			cv2.rectangle(frame, p1,p2, (0,0,255),2,2) # Print tracking rectangle on stream
+			cv2.imshow("Tracking", frame)  # Show stream
 
 		t0=time.time()
-		while not ok: # On a perdu la personne
+		while not ok: # Face lost
 			ok, frame = cam.read()
 			cv2.imshow("Tracking", frame)
-			if(time.time()-t0)>5: # Si cela fait plus de 5s, il faut relancer le tracking
+			if(time.time()-t0)>5: # If 5s has passed and still no face detected
 				print("process time")
-				tracker = cv2.TrackerKCF_create() # On recrée un tracker
+				tracker = cv2.TrackerKCF_create() # We recreate a tracker
 				bbox =init(frame)
 				while bbox == (0,0,0,0) :
-					print("bloqué")
+					print("no face detected")
 					ok, frame = cam.read()
 					bbox=init(frame)
-					cv2.imshow("Tracking", frame) # On essaie de retrouver la personne
-				ok = tracker.init(frame, bbox)  # On réinitialise le tracker lorsqu'on a trouvé la personne
-			ok, bbox = tracker.update(frame) # On met à jour le tracker
+					cv2.imshow("Tracking", frame) # Get stream whil searching
+				ok = tracker.init(frame, bbox)  # Tracker reinitialization on detection
+			ok, bbox = tracker.update(frame) # Tracker update
 
 
 		k = cv2.waitKey(1) & 0xFF
-		if k == 27: # echap pour quitter le programme. 
+		if k == 27: # esc to quit. 
 				print("ESC hit, closing...")
 				break
 
