afd5c112d42caf4b85d967724c10890cdaa9fc4e
PCov3r <38764918+PCov3r@users.noreply.github.com>
2021-06-13 22:34:54 +0200

    Delete WebServer/clientSide directory

--- WebServer/clientSide/api.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <curl/curl.h>
-
-char* activateOverride(void);
-char* sendCmd(int, int, int);
-size_t write_callback_func(void, size_t, size_t, void);
